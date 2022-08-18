f717642a775c693ac4b270437f140514c32642b9
Nikita <djneel.nz@gmail.com>
2022-08-18 18:11:40 +0300

    generate socks ready for logic

--- src/components/SocksGenerator.jsx
+++ src/components/SocksGenerator.jsx
@@ -6,9 +6,9 @@ export default function SocksGenerator() {
       <h1>Socks Generator</h1>
       <div className="colorBox">
         <img className="generateMask" src="/images/socks_mask.png" alt="Emply socks mask" />
+        <div className="imagesSocks"><img className="imagesSocks" src="/images/рис4.png" alt="Узор4" /></div>
         <div className="colorSocks" />
-        <div className="patternSocks" />
-        <div className="imagesSocks" />
+        <div className="patternSocks"><img className="patternSocks" src="/images/4.png" alt="Узор4" /></div>
       </div>
     </div>
   );
